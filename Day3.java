37a11857dceb00a1f8c5cd855767d186bb655c9c
IhksanIccan <146822945+IhksanIccan@users.noreply.github.com>
2023-10-11 16:22:48 +0800

    Create Day3.java
    Tipe data char dan boolean
    . tipe data char hanya dapat menampung 1 karakter dan di tandai kutip 1(')
    .boolean itu ada 2 yaitu true dan false (benar/salah)
    Maaf kak yg day 2 itu saya ketik semua yg tipe datanya🙏

---
+++ Day3.java
@@ -0,0 +1,30 @@
+package Day3;
+
+public class day3 {
+    
+    public static void main(String[] args) {
+        // TIPE DATA CHARAKTER (CHAR)
+        char Huruf1 = 'I'; //char hanya bisa menampung 1 karakter
+        char Huruf2 = 'H';
+        char Huruf3 = 'K';
+        char Huruf4 = 'S';
+        char Huruf5 = 'A';
+        char Huruf6 = 'N';
+        
+        System.out.println(Huruf1);
+        System.out.println(Huruf2);
+        System.out.println(Huruf3);
+        System.out.println(Huruf4);
+        System.out.println(Huruf5);
+        
+     //Tipe data boolean
+     //Tipe data boolean hanya mempunyai 2 nilai(benar/salah)
+        boolean benar = true;
+        boolean salah = false;
+        
+        System.out.println(benar);
+        System.out.println(salah);
+        
+    }
+    
+}
