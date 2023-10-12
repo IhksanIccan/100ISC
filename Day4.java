83303ad4996e101dd475caef4b9d838477e4890e
IhksanIccan <146822945+IhksanIccan@users.noreply.github.com>
2023-10-12 10:49:36 +0800

    Create Day4.java
    Kodingan saya hari ini kak yaitu
    Menyusun kalimat pakai String na int kak🙏

---
+++ Day4.java
@@ -0,0 +1,21 @@
+package hari4;
+
+public class hari4 {
+    
+    public static void main(String[] args) {
+        String nama = "Ihksan";
+        String Nim = "D0223049";
+        String Prodi = "Teknik Informatika";
+        String Kelas = "Informatika B 2023";
+        int Umur = 19;
+        String Wa = "082194842194";
+        
+        System.out.println("Assalamualaikum Wr.Wb,Perkenalkan Nama saya:"+nama+",Nim:"+Nim);
+        System.out.println("Prodi:"+Prodi+",kelas:"+Kelas+",Umur:"+Umur+"tahun");
+        System.out.println("No.wa:"+Wa);
+        System.out.println("Saya atas nama "+nama+"ingin masuk ISC itu karna lebih ingin dalam");
+        System.out.println("mengetahui perkodingan.Dan saya atas "+nama+",Insaallah akan sanggup");
+        System.out.println("menjalani ISC dalam 100 Day ini!! ^-^");
+    }
+    
+}
