0d46b1bffa3385682c789a4309af688ed90e013e
IhksanIccan <146822945+IhksanIccan@users.noreply.github.com>
2023-10-13 14:56:01 +0800

    Create Day5.java
    Assalamualaikum Wr.Wb
    Kali ini saya akan mengeksplorasi menggunakan Escape Squences pada pemrograman java.
    Yaitu:
    \t -> sebagai Tab
    \n -> sebagai Enter
    
    Sampai disini yang saya ketahui kak🙏

---
+++ Day5.java
@@ -0,0 +1,37 @@
+package day5;
+
+public class day5 {
+    
+    public static void main(String[] args) {
+        // Assalmualaikum
+        // Saya disini akan mencoba merapikan kalimat
+        // menggunakan Karakter Istimewa/Escape Squences
+            // Contoh tampa menggunakan Escape Squences //
+        System.out.println("Contoh di bawa yang belum tersusun rapih");
+        String nama = "IHKSAN";
+        int umur = 19;
+        double beratBadan = 71.5;
+        System.out.println("nama = "+ nama );
+        System.out.println("umur = "+ umur + "tahun");
+        System.out.println("Berat Badan = "+beratBadan + "kg");
+        
+        // Sekarang kita menggunakan Escape Squences/Karekter Istimewa
+            //Contoh menggunakan Karakter Istimewa/Escape Squences//
+        System.out.println("Contoh di bawah yang tersusun rapih");
+        String Nama = "IHKSAN";
+        int usia = 19;
+        double BeratBadan = 71.5;
+        
+        // Escape Squences/Karekter Istimewa
+        /*
+        \t -> tab 
+        \n -> enter / baris baru
+        */
+        
+         System.out.println("Nama \t\t= "+ Nama+"\nUsia \t\t= "+ usia + "tahun" );
+         System.out.println("Berat Badan \t= "+BeratBadan + "kg");
+         
+                 // SEKIAN SAMPAI DISINI YANG SAYA PELAJARI KAK >< //
+    }
+    
+}
