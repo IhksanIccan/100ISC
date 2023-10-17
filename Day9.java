0668df245074ab8457f129458421edcf01622fec
IhksanIccan <146822945+IhksanIccan@users.noreply.github.com>
2023-10-17 21:40:01 +0800

    Create Day9.java
    Operator Penugasan (Operator Assignment)
    kali ini saya akan mengulas tentang pemanfaatan operator assignment pada pemrograman java.
    Contoh:
    a = 2
    b = 3
    a += b;
    Atau di sebut (a = a + b)
    2 (hasil dari a)
    5 (hasil dari a + b / 2 + 5)
    
    Sekian yg saya buat kak,kalau ada salah mohon di koreksi kak🙏🙏

---
+++ Day9.java
@@ -0,0 +1,65 @@
+package day9;
+public class day9 {
+    public static void main(String[] args) {
+        /*
+        
+        =
+        +=
+        -=
+        /=
+        %=
+        
+        */
+       // => =
+       // int angka;
+       // angka = 11;
+       // System.out.println(angka);
+       // angka = 56;
+       // System.out.println(angka);
+       System.out.println("Hasil Dari Perjumlahan ===============");   
+         // => +=
+        int a, b;
+        a = 4;
+        b = 7;
+        System.out.println(a);
+        a += 2;
+        // a = a + 2;
+        System.out.println(a);
+        System.out.println("Hasil Dari Pengurangan ===============");
+        // => -=
+        double c, d;
+        c = 8;
+        d = 2;
+        System.out.println(c);
+        c -= b;
+        System.out.println(c);
+        System.out.println("Hasil Dari Pembagian ===============");
+         // => /=
+        int e, w;
+        e = 50;
+        w = 5;
+        System.out.println(e);
+        e /= w;
+        System.out.println(e);
+        System.out.println("Hasil Dari Perkalian ===============");
+          // => /=
+        int u, i;
+        u = 8;
+        i = 2;
+        System.out.println(u);
+        u*= i;
+        System.out.println(u);
+        System.out.println("Hasil Dari Sisa bagi ===============");
+          // => /=
+        int g, h;
+        g = 100;
+        h = 3;
+        System.out.println(g);
+        g %= h;
+        System.out.println(g);
+            //Sekian yang saya pelajari kak//
+        
+                
+    }
+    
+}
