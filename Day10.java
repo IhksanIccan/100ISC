5be0da4a25db06c8fd0202c9ac38bbce1ca13df5
IhksanIccan <146822945+IhksanIccan@users.noreply.github.com>
2023-10-18 19:06:53 +0800

    Create Day10.java
    Operator Pembanding pada Java
    [==] [=!] [>] [>=] [<] [<=]
    Saya kali ini  akan mengulas tentang macam-macam operator pembanding pada pemrograman java.

---
+++ Day10.java
@@ -0,0 +1,52 @@
+package Day10_Percabangan;
+
+public class Day10 {
+    
+    public static void main(String[] args) {
+        /*
+        == (senilai)
+        != (tidak senilai)
+        >					(lebih dari)
+        >= (lebih dari atau sama dengan)
+        <  (kurang dari)
+        <= (kurang dari atau sama dengan)
+        */
+        System.out.println("\tSENILAI");
+        System.out.println("Int a =11;\nInt b =11;\njadi 11 dan 11 itu senilai");
+        int a = 11;
+        int b = 11;
+        System.out.println(a == b);
+        System.out.println("\n");
+        System.out.println("\tTIDAK SENILAI");
+        System.out.println("Int c =11;\nInt d =12;\njadi 11 dan 12 itu tidak senilai");
+        int c = 11;
+        int d = 12;
+        System.out.println(c != d);
+        System.out.println("\n");
+        System.out.println("\tLEBIH DARI");
+        System.out.println("Int e =32;\nInt f =16;\njadi 32 itu lebih besar dari 16");
+        int e = 32;
+        int f = 16;
+        System.out.println(e > f);
+        System.out.println("\n");
+        System.out.println("\tLEBIH DARI ATAU SAMA DENGAN");
+        System.out.println("Int g =9;\nInt h =8;\njadi 9 lebih dari sama dengan 8");
+        int g = 9;
+        int h = 8;
+        System.out.println(g >= h);
+        System.out.println("\n");
+        System.out.println("\tKURANG DARI");
+        System.out.println("Int i =9;\nInt j =11;\njadi 9 itu lebih kecil dari 11");
+        int i = 9;
+        int j = 11;
+        System.out.println(i < j);
+        System.out.println("\n");
+        System.out.println("\tKURANG DARI ATAU SAMA DENGAN");
+        System.out.println("Int k =11;\nInt l =14;\njadi 11 itu kurang dari sama dengan 14");
+        int k = 11;
+        int l = 14;
+        System.out.println(k <= l);
+        // Sekian yang saya pelajari saat Ini kak
+    }     
+    
+}
